7c865d95f9c75b9dac3f2933ca64b5ab9b32b1ab
Noelani-Diamond <nceballos26@aoiths.org>
2025-01-12 23:27:10 -0500

    More enemies spawn and more powerups

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,5 +20,9 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = ((player.transform.position - transform.position).normalized * speed);
         enemyRb.AddForce(lookDirection * speed);
+        if ( transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
